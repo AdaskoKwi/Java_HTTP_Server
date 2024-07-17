f5bfbd47cd34bf3444fc95548d3648ef421788b7
AdaskoKwi <adamnowakhomm@gmail.com>
2024-07-17 12:05:40 +0200

    Initial commit

---
+++ src/SimpleHTTPServer.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+public class SimpleHTTPServer {
+    public static void main(String[] args) throws IOException {
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080...");
+        while (true){
+            try(Socket socket = server.accept()){
+                Date today = new Date();
+                String HTTPresponse = "HTTP/1.1/ 200 OK \r\n\r\n" + today;
+
+                socket.getOutputStream()
+                        .write(HTTPresponse.getBytes(StandardCharsets.UTF_8));
+            }
+        }
+    }
+}
